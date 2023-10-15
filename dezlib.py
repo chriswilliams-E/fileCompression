951c722bdd81546a8bdca9d9e93c0a8bda6ce096
Christopher <chriswilliams.lax3@gmail.com>
2023-10-14 22:30:32 -0700

    initial commit of crappy code

---
+++ dezlib.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import zlib
+
+fileName = input('Enter name of zlib file: ' )
+
+compzFile = open(fileName, 'rb').read()
+print('Deflating %s' % fileName)
+deflateData = zlib.compress(compzFile, zlib.Z_BEST_COMPRESSION)
+#print(len(compzFile))
+compress_ratio = (float(len(compzFile)) - float(len(deflateData))) / float(len(compzFile))  
+print('Compressed: %d%%' % (100.0 * compress_ratio))
+
+compressedOutFile = open(fileName + '.dat', 'w')
+compressedOutFile.write(str(deflateData))
+compressedOutFile.close()
+'''
+zFile = open(fileName, 'rb')
+print('Inflating %s' % fileName)
+inflateData = zlib.decompress(zFile.read())
+zFile.close()
+
+inflatedFile = open('%s-df' % fileName, 'w')
+print('Inflated data is being written to: %s' % inflatedFile)
+inflatedFile.write(inflateData)
+inflatedData.close()
+'''
